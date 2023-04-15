491c7ec103238d3822cef5cfb0b5e2c8aa151fd7
jamesly celestin <jamesly.celestin33@gmail.com>
2023-04-15 12:24:43 -0400

    Install node package and import libraries

---
+++ index.js
@@ -0,0 +1,7 @@
+import sha256 from 'crypto-js/sha256';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
+import Base64 from 'crypto-js/enc-base64';
+
+
+var AES = require("crypto-js/aes");
+var SHA256 = require("crypto-js/sha256");
